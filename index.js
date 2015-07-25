011e7f4812fe7ac81aa658d04d88ba056c9150f2
Aled Lewis <github@aled-lewis.co.uk>
2015-07-25 12:47:14 +0100

    re-jigged status object generation

--- index.js
+++ index.js
@@ -18,23 +18,19 @@ var cmd_sr_run = 'java -jar "{0}" -run {1} -jdbc {2} -user {3} -password {4} -lo
 
 exports.get_instance= function(config){
 return function(changed_file_path, outcome) {
+
   var result;
   var runid = uuid.v1();  
-  console.log(changed_file_path);
   var fileRelativeDir = path.relative(config.scriptrunner.codeSourcePath,path.dirname(changed_file_path));
-  
-
-
 
   temp.mkdir(clobber_dir_name, function(err, target_working_dir){
+
     if (err) throw err;
     var target_build_dir = path.join(target_working_dir, 'build');
-    //make the directory to the changed file
     var target_build_file_path = path.join(target_build_dir, fileRelativeDir, path.basename(changed_file_path));
 
     
     fs.ensureFile(target_build_file_path, function(err){
-      console.log(err);
       fs.copySync(changed_file_path, target_build_file_path);
     });
    
@@ -63,29 +59,28 @@ return function(changed_file_path, outcome) {
       config.scriptrunner.password, target_working_dir
     );
     
-    exec(build_cmd, function(err) {
+    exec(build_cmd, function(err,stdout,stderr) {
+      var exec_result = {"file_location":changed_file_path, "build_output":stdout};
+
       if (err) {
-        outcome({
-          "result":"error",
-          "file_location":changed_file_path,
-          "err":err
-        });
+        exec_result.result= "error";
+        exec_result.err = err;
+        exec_result.err_out = stderr;
+        outcome(exec_result);
       }
-      exec(run_cmd, function(err) {
+      
+      exec(run_cmd, function(err,stdout, stderr) {
+        exec_result.run_output=stdout;
+
         if (err) {
-          outcome({
-            "result":"error",
-            "file_location":changed_file_path,
-            "err":err
-          });
-        }
-        fs.emptyDirSync(target_working_dir);
-        fs.rmdirSync(target_working_dir);
-        outcome({
-          "result":"success",
-          "file_location":changed_file_path,
-          "build_location":target_working_dir
-        });
+          exec_result.result= "error";
+          exec_result.err = err;
+          exec_result.err_out = stderr;
+          outcome(exec_result);
+        } else {
+          exec_result.result = "success";
+          outcome(exec_result);
+        }      
       });
     
     });
